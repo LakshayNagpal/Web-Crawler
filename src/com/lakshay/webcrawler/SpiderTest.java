e1be90a1f5bbff812f89f1e634a55dfaaba94174
Lakshay Nagpal <lakshay.nagpal00000@gmail.com>
2015-06-20 12:12:50 +0530

    First Commit

---
+++ src/com/lakshay/webcrawler/SpiderTest.java
@@ -0,0 +1,16 @@
+package com.lakshay.webcrawler;
+
+public class SpiderTest {
+
+	/**
+     * This is our test. It creates a spider (which creates spider legs) and crawls the web.
+     * 
+     * @param args
+     *            - not used
+     */
+    public static void main(String[] args)
+    {
+        Spider spider = new Spider();
+        spider.crawl("http://arstechnica.com/", "computer");
+    }
+}
